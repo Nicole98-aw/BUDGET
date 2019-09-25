88991d426bcc7a5472e793ba438a1bd3a2004f4e
Nicole98_aw <nicoleotieno18@gmail.com>
2019-09-25 16:03:45 +0300

    added actions and reducers

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import AddExpensePage from "../../components/AddExpensePage";
+
+test("should render AddExpensePage correctly", () => {
+  const wrapper = shallow(<AddExpensePage />);
+  expect(wrapper).toMatchSnapshot();
+});
---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import EditExpensePage from "../../components/EditExpensePage";
+
+test("should render EditExpensePage correctly", () => {
+  const wrapper = shallow(<EditExpensePage />);
+  expect(wrapper).toMatchSnapshot();
+});
