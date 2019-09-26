14c74f051b6b5173f76ebf906823183fd5a34639
Nicole98_aw <nicoleotieno18@gmail.com>
2019-09-26 09:21:04 +0300

    add readme file

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,11 @@
-import React from 'react'; 
-import ExpenseForm from './ExpenseForm';
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 const AddExpensePage = () => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm />
-    </div>
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
 );
 
 export default AddExpensePage;
