fa38d8fe31e6675f1fca7318a0b1b553ab1ec319
Nicole98_aw <nicoleotieno18@gmail.com>
2019-09-26 09:46:05 +0300

    added input in ExpenseForm.js

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,35 +1,35 @@
-import React from 'react';
+import React from "react";
 
 export default class ExpenseForm extends React.Component {
-    state = {
-        description: 'coffee',
-        note: 'vancouver',
-        amount: '8000'
-    };
+  state = {
+    description: "coffee",
+    note: "vancouver",
+    amount: "8000"
+  };
 
-    render() {
-        return (
-            <div>
-                <form>
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={this.state.description}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Amount"
-                        autoFocus
-                        value={this.state.amount} 
-                    
-                    />
-                    <textarea
-                          placeholder="Add a note for your expense(optional)"
-                          value={this.state.note}></textarea>
-                    <button>Add Expense</button>           
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+          />
+          <input
+            type="text"
+            placeholder="Amount"
+            autoFocus
+            value={this.state.amount}
+          />
+          <textarea
+            placeholder="Add a note for your expense(optional)"
+            value={this.state.note}
+          ></textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    );
+  }
 }
