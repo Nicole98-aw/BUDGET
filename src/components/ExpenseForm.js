70009d1a32f32bea377ee8dc26c4fe7e91c640d6
Nicole98_aw <nicoleotieno18@gmail.com>
2019-09-27 10:12:07 +0300

    addedonChange event in ExpenseForm.js

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -7,6 +7,23 @@ export default class ExpenseForm extends React.Component {
     amount: "8000"
   };
 
+  onDescriptionChange = (e) => {
+    const description =e.target.value;
+    this.setState(() => ({ description}));        
+  }
+
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  } 
+
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+
+    if(amount.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }))
+    }
+  }
   render() {
     return (
       <div>
@@ -16,16 +33,19 @@ export default class ExpenseForm extends React.Component {
             placeholder="Description"
             autoFocus
             value={this.state.description}
+            onChange={this.onDescriptionChange}
           />
           <input
             type="text"
             placeholder="Amount"
             autoFocus
             value={this.state.amount}
+            onChange={this.onAmountChange}
           />
           <textarea
             placeholder="Add a note for your expense(optional)"
             value={this.state.note}
+            onChange={this.onNoteChange}
           ></textarea>
           <button>Add Expense</button>
         </form>
