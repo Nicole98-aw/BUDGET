5b698856aae54c709f2bb88bfbe4b998c7329bbb
Nicole <nicoleotieno18@gmail.com>
2019-10-11 12:42:22 +0100

    modified all files

---
+++ src/firebase/firebase.js
@@ -0,0 +1,38 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref("expenses").push({
+//   description: "Rent",
+//   note: "",
+//   amount: 109500,
+//   createdAt: 9761234988763
+// });
+
+// database.ref("expenses").push({
+//   description: "Phone bill",
+//   note: "",
+//   amount: 5900,
+//   createdAt: 9761234988763
+// });
+
+// database.ref("expenses").push({
+//   description: "Food",
+//   note: "",
+//   amount: 1200,
+//   createdAt: 9761234988763
+// });
